ac4f6c9296c3120db5ac17da658a525bdb89e420
MasonJArnold <mason.jarnold@gmail.com>
2020-06-29 18:56:07 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,13 +4,13 @@
        // The base url to your WP site.
        baseUrl: 'https://gatsbyreact.wpengine.com',
        // WP.com sites set to true, WP.org set to false
-       hostingWPCOM: false,
+       hostingWPCOM: true,
        // The protocol. This can be http or https.
        protocol: 'http',
        // Use 'Advanced Custom Fields' Wordpress plugin
        useACF: true,
        auth: {},
        // Set to true to debug endpoints on 'gatsby build'
-       verboseOutput: false
+       verboseOutput: true
      }
    },```
