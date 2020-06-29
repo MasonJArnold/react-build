1958826f3d3c91f6f4ca315f232b341883db62cd
MasonJArnold <mason.jarnold@gmail.com>
2020-06-29 17:09:59 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,7 +40,7 @@ module.exports = {
         // The protocol. This can be http or https.
         protocol: 'https',
         // Use 'Advanced Custom Fields' Wordpress plugin
-        useACF: true,
+        useACF: false,
         // Set to true to debug endpoints on 'gatsby build'
         verboseOutput: false,
         includedRoutes: [
